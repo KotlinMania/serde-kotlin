078bca51cfc70fb55ee1004c6f5ba9031fa67173
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-07 21:01:25 -0700

    Wire serde derive dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,3 +18,29 @@ dependencyResolutionManagement {
 
 rootProject.name = "serde-kotlin"
 
+val procMacro2Local = file("../proc-macro2-kotlin")
+if (procMacro2Local.exists()) {
+    includeBuild(procMacro2Local) {
+        dependencySubstitution {
+            substitute(module("io.github.kotlinmania:proc-macro2-kotlin")).using(project(":"))
+        }
+    }
+}
+
+val quoteLocal = file("../quote-kotlin")
+if (quoteLocal.exists()) {
+    includeBuild(quoteLocal) {
+        dependencySubstitution {
+            substitute(module("io.github.kotlinmania:quote-kotlin")).using(project(":"))
+        }
+    }
+}
+
+val synLocal = file("../syn-kotlin")
+if (synLocal.exists()) {
+    includeBuild(synLocal) {
+        dependencySubstitution {
+            substitute(module("io.github.kotlinmania:syn-kotlin")).using(project(":"))
+        }
+    }
+}
