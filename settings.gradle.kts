711d6c76c2b3da9e5f4dc1ce57a74ee569bbe666
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-13 13:45:37 -0700

    build: remove local composite builds, add mavenLocal for published deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,42 +11,10 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 
 dependencyResolutionManagement {
     repositories {
+        mavenLocal()
         google()
         mavenCentral()
     }
 }
 
 rootProject.name = "serde-kotlin"
-
-fun findLocalDependencyDir(name: String): File? =
-    listOf(
-        file("../$name"),
-        file("deps/$name"),
-    ).firstOrNull { it.exists() }
-
-val procMacro2Local = findLocalDependencyDir("proc-macro2-kotlin")
-if (procMacro2Local != null) {
-    includeBuild(procMacro2Local) {
-        dependencySubstitution {
-            substitute(module("io.github.kotlinmania:proc-macro2-kotlin")).using(project(":"))
-        }
-    }
-}
-
-val quoteLocal = findLocalDependencyDir("quote-kotlin")
-if (quoteLocal != null) {
-    includeBuild(quoteLocal) {
-        dependencySubstitution {
-            substitute(module("io.github.kotlinmania:quote-kotlin")).using(project(":"))
-        }
-    }
-}
-
-val synLocal = findLocalDependencyDir("syn-kotlin")
-if (synLocal != null) {
-    includeBuild(synLocal) {
-        dependencySubstitution {
-            substitute(module("io.github.kotlinmania:syn-kotlin")).using(project(":"))
-        }
-    }
-}
