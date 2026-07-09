5f8eb6822c3c64c5eb188615365006ebf87bb9ba
Sydney Renee <sydney@solace.ofharmony.ai>
2026-07-09 15:42:46 -0700

    Fix serde derive const wrapper quoting

--- serde_derive/src/commonMain/kotlin/io/github/kotlinmania/serderive/internals/Dummy.kt
+++ serde_derive/src/commonMain/kotlin/io/github/kotlinmania/serderive/internals/Dummy.kt
@@ -8,28 +8,28 @@ public fun wrapInConst(serdePath: Path?, code: TokenStream): TokenStream {
     val useSerde = if (serdePath != null) {
         quote("""
             use `#`serdePath as _serde;
-        """)
+        """, mapOf("serdePath" to serdePath))
     } else {
         quote("""
-            `#`[allow(unused_extern_crates, clippy.useless_attribute)]
+            #[allow(unused_extern_crates, clippy::useless_attribute)]
             extern crate serde as _serde;
         """)
     }
 
     return quote("""
-        `#`[doc(hidden)]
-        `#`[allow(
+        #[doc(hidden)]
+        #[allow(
             non_upper_case_globals,
             unused_attributes,
             unused_qualifications,
-            clippy.absolute_paths,
+            clippy::absolute_paths,
         )]
         const _: () = {
             `#`useSerde
 
-            _serde.__require_serde_not_serde_core!();
+            _serde::__require_serde_not_serde_core!();
 
             `#`code
         };
-    """)
+    """, mapOf("useSerde" to useSerde, "code" to code))
 }
